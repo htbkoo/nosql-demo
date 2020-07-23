6d74f61f6ff00b3e80519eab5895f5540b1d2831
Broderick Koo <broderick.koo@cloverhealth.com>
2020-07-24 00:04:38 +0800

    (WIP) created `systemDesignPractice.ts` and test

---
+++ src/typescript/systemDesignPractice.test.ts
@@ -0,0 +1,14 @@
+import { db } from "./systemDesignPractice";
+
+describe("System Design interview blog system", function () {
+  describe("doctors", () => {
+    it("should get existing doctor information", async () => {
+      // given
+      // when
+      const docs = await db.get("doctors");
+
+      // then
+      expect(docs).toEqual([]);
+    });
+  });
+})
---
+++ src/typescript/systemDesignPractice.ts
@@ -0,0 +1,5 @@
+import * as PouchDB from "pouchdb";
+
+const db = new PouchDB('my_database');
+
+export { db };
