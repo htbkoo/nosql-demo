753b5311e9a84f743c42f4aceb74f7f4d9567359
htbkoo <h1017760@connect.hku.hk>
2020-07-24 00:56:59 +0800

    applied auto-formatting

--- src/typescript/systemDesignPractice.test.ts
+++ src/typescript/systemDesignPractice.test.ts
@@ -1,14 +1,14 @@
 import { db } from "./systemDesignPractice";
 
 describe("System Design interview blog system", function () {
-  describe("doctors", () => {
-    it("should get existing doctor information", async () => {
-      // given
-      // when
-      const docs = await db.get("doctors");
+    describe("doctors", () => {
+        it("should get existing doctor information", async () => {
+            // given
+            // when
+            const docs = await db.get("doctors");
 
-      // then
-      expect(docs).toEqual([]);
+            // then
+            expect(docs).toEqual([]);
+        });
     });
-  });
 })
