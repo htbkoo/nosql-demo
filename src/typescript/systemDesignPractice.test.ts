33ae84b67e08ae615d2141d0d06b3aa84d5bfc98
htbkoo <h1017760@connect.hku.hk>
2020-07-24 11:36:51 +0800

    added more test case

--- src/typescript/systemDesignPractice.test.ts
+++ src/typescript/systemDesignPractice.test.ts
@@ -1,8 +1,8 @@
-import { getDoctorWithIdEqualsToDr2 } from "./systemDesignPractice";
+import { getDoctorWithIdEqualsToDr2, getDoctorWithNameEqualsToDoctor3 } from "./systemDesignPractice";
 
 describe("System Design interview blog system", function () {
     describe("doctors", () => {
-        it("should get existing doctor information", async () => {
+        it("should get the doc for doctor with docId=dr2", async () => {
             // given
             // when
             const docs = await getDoctorWithIdEqualsToDr2();
@@ -11,6 +11,18 @@ describe("System Design interview blog system", function () {
             expect(docs).toMatchObject({
                 _id: "dr2",
                 name: "doctor2",
+            });
+        });
+
+        it("should get the doc for doctor with name=doctor3", async () => {
+            // given
+            // when
+            const docs = await getDoctorWithNameEqualsToDoctor3();
+
+            // then
+            expect(docs).toMatchObject({
+                _id: "dr3",
+                name: "doctor3",
             });
         });
     });
