c51189665cf72f3f91901c54a43eb10f1eb041b4
htbkoo <h1017760@connect.hku.hk>
2020-07-24 11:30:02 +0800

    (WIP)

--- src/typescript/systemDesignPractice.test.ts
+++ src/typescript/systemDesignPractice.test.ts
@@ -1,11 +1,11 @@
-import { dbDoctors } from "./systemDesignPractice";
+import { getDoctorWithIdEqualsToDr2 } from "./systemDesignPractice";
 
 describe("System Design interview blog system", function () {
     describe("doctors", () => {
         it("should get existing doctor information", async () => {
             // given
             // when
-            const docs = await dbDoctors.get("doctors");
+            const docs = await getDoctorWithIdEqualsToDr2();
 
             // then
             expect(docs).toEqual([]);
--- src/typescript/systemDesignPractice.ts
+++ src/typescript/systemDesignPractice.ts
@@ -4,23 +4,29 @@ function exampleForGettingDocFromDbDoctors() {
     return dbDoctors.get("dr1").then(doc => console.log(doc));
 }
 
+async function exampleForGettingDocFromDbDoctorsAsAsyncFunction() {
+    const doc = await dbDoctors.get("dr1");
+    return console.log(doc);
+}
+
 // TODO ------------------------------
 // TODO: START - implement the functions below!
+// TODO: feel free to not use `await` if you found it unfamiliar
 // TODO ------------------------------
 
-export function getDoctorWithIdEqualsToDr2() {
+export async function getDoctorWithIdEqualsToDr2() {
     // TODO: implement this function
     //  we want to get the doc for `Doctor` with docId equals to "dr2"
 
 }
 
-export function getDoctorWithNameEqualsToDoctor3() {
+export async function getDoctorWithNameEqualsToDoctor3() {
     // TODO: implement this function
     //  we want to get the doc for `Doctor` with name equals to "Doctor 3"
 
 }
 
-export function getAllDoctorsInDB() {
+export async function getAllDoctorsInDB() {
     // TODO: implement this function
     //  we want to get all `Doctor` doc in the DB
 }
