267abfc985f7e111e9a4b0211c101a53326a641c
htbkoo <h1017760@connect.hku.hk>
2020-07-24 11:27:24 +0800

    added `pouchdb-find` as pouchdb plugin

--- src/typescript/infrastructures/pouchdbUtils.ts
+++ src/typescript/infrastructures/pouchdbUtils.ts
@@ -1,6 +1,7 @@
 import * as PouchDB from "pouchdb";
 
 PouchDB.plugin(require('pouchdb-adapter-memory'));
+PouchDB.plugin(require('pouchdb-find'));
 
 export function newInMemoryPouchdb(name, extraOptions = {}) {
     return new PouchDB(name, { adapter: 'memory', ...extraOptions });
